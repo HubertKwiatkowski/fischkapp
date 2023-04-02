818923a621281ed2396cd5b60d26daefa137fb19
Hubert <h.kwiatkowski@hotmail.com>
2023-04-02 23:53:35 +0200

    chore: card-flip

--- js/ui/card.js
+++ js/ui/card.js
@@ -5,26 +5,36 @@ export const getCardComponent = (params) => {
 
   const handleToggleCard = () => {
     state = { ...state, isFront: !state.isFront };
-    card.querySelector('.text').innerText = state.isFront
-      ? params.front
-      : params.back;
+
+    card.querySelector('.flipper').classList.toggle('flip');
+    card.querySelector('.flipper').classList.toggle('flipped');
+    const list = card.querySelectorAll('.fading');
+    for (const element of list) {
+      element.classList.toggle('fade');
+      element.classList.toggle('faded');
+    }
+    setTimeout(() => {
+      card.querySelector('.text').innerText = state.isFront
+        ? params.front
+        : params.back;
+    }, 250);
   };
 
   const template = `
-  <div
-    class="card-front"
-  >
-    <div class="edit-icon">
+  <div class="card-wrapper">
+    <div class="flipper flip card-front"></div>
+    <div class="edit-icon fade fading">
       <img
         src="src/icon/edit-icon.svg"
         alt="edit-icon"
       />
     </div>
-    <p class="text"></p>
+    <p class="text fade fading"></p>
   </div>
   `;
 
   const card = document.createElement('div');
+  card.classList.add('flip');
 
   card.innerHTML = template.trim();
   card.addEventListener('click', handleToggleCard);
