d72757cc46ecbff23582d463bdde32352e97152f
Hubert <h.kwiatkowski@hotmail.com>
2023-04-07 20:44:46 +0200

    fix: code cleanup

--- js/ui/cardList.js
+++ js/ui/cardList.js
@@ -2,13 +2,9 @@ import { getCardComponent } from './cardComponent.js';
 
 export const getCardList = (params) => {
   const cardList = document.getElementById('card-list');
-  console.log(params.flashcards);
-  console.log(typeof params.flashcards);
   while (cardList.firstChild) cardList.firstChild.remove();
-  const cardsAmount = params.flashcards.length;
 
   params.flashcards.forEach((flashcard) => {
-    console.log(flashcard);
     const card = getCardComponent(flashcard);
     cardList.append(card);
   });
