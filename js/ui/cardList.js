91afe1ffdbe1aee1d7a565016c108833ddb7c791
Hubert <h.kwiatkowski@hotmail.com>
2023-04-07 20:15:04 +0200

    fix: fixed getCardList

--- js/ui/cardList.js
+++ js/ui/cardList.js
@@ -1,11 +1,9 @@
 import { getCardComponent } from './cardComponent.js';
 
-const cardList = document.getElementById('card-list');
-
 export const getCardList = (params) => {
+  const cardList = document.getElementById('card-list');
   const card = getCardComponent(params.flashcards[0]);
 
   while (cardList.firstChild) cardList.firstChild.remove();
-
   cardList.append(card);
 };
